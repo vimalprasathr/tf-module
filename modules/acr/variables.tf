dcf72796b41b7bc3d08d70cb6916a8c451019a8b
Vimal Prasath <44838054+vimalprasathr@users.noreply.github.com>
2025-07-09 20:03:01 +0530

    Update variables.tf

--- modules/acr/variables.tf
+++ modules/acr/variables.tf
@@ -1,32 +1,32 @@
-variable "resource_group_name" {
+variable "acr_name" {
+  description = "Name of the Azure Container Registry"
   type        = string
-  description = "Name of the resource group"
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
 }
 
 variable "location" {
+  description = "Azure location"
   type        = string
-  description = "Azure region where resources will be created"
-}
-
-variable "acr_name" {
-  type        = string
-  description = "Globally unique name for the Azure Container Registry"
 }
 
 variable "acr_sku" {
+  description = "The SKU of the ACR (Basic, Standard, Premium)"
   type        = string
-  description = "SKU for the ACR (Basic, Standard, Premium)"
   default     = "Basic"
 }
 
 variable "admin_enabled" {
+  description = "Enable admin access"
   type        = bool
-  description = "Enable or disable admin user access"
   default     = false
 }
 
 variable "tags" {
+  description = "Tags for the ACR"
   type        = map(string)
-  description = "Tags to apply to all resources"
   default     = {}
 }
