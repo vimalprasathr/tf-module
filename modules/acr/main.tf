f5c7a2546ae7f8c8ff83406df5e6d651b45b40fc
Vimal Prasath <44838054+vimalprasathr@users.noreply.github.com>
2025-07-09 19:44:22 +0530

    Update main.tf

--- modules/acr/main.tf
+++ modules/acr/main.tf
@@ -1,12 +1,8 @@
-resource "azurerm_container_registry" "this" {
-  name                = var.acr_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  sku                 = var.acr_sku
-  admin_enabled       = var.admin_enabled
-  tags                = var.tags
-
-  lifecycle {
-    prevent_destroy = false
-  }
+resource "azurerm_container_registry" "acr" {
+  name                     = var.acr_name
+  location                 = var.location
+  resource_group_name      = azurerm_resource_group.acr.name
+  sku                      = var.acr_sku
+  admin_enabled            = var.admin_enabled
+  tags                     = var.tags
 }
